37043aff9818846af2946a2a125cb51a7b2e8607
Dawid <davi12396@gmail.com>
2019-04-06 17:29:53 +0200

    HERE maps done

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,17 @@
 const restApiHelpers = require('./restapi_helpers');
 const http = require('http');
+const fs = require('fs')
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  
+  // Read HTML to return
+  fs.readFile('map.html', {encoding: 'utf-8'}, function(err,data){
+      if (!err) {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+      } else {
+          console.log(err);
+      }
+  });
 }).listen(8080);
