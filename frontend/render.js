4445b87d0462f0a3790f73580da6b9d6fa840276
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-04-06 21:17:02 +0200

    graph rendering

---
+++ frontend/render.js
@@ -0,0 +1,39 @@
+function renderChart(data, labels, nodeId) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: `Node ${nodeId}`,
+          data: data
+        }
+      ]
+    }
+  });
+}
+
+var nodeId = 20;
+
+$.ajax({
+  url: "http://localhost:5000/trigger_calc",
+  type: "GET",
+  dataType: "json",
+  success: function(result, nodeId) {
+    renderData(result);
+  },
+  error: function(result) {
+    console.log(result.status + " " + result.statusText);
+  }
+});
+
+function renderData(result) {
+  data = result["nodes"][nodeId]["history"]["today"];
+  labels = [];
+  for (i = 0; i < data.length; i++) {
+    labels.push(i);
+  }
+
+  renderChart(data, labels, nodeId);
+}
